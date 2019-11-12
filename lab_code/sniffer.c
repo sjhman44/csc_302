6c4c033dea7b40cf8c772c9bcf87c223182802d1
sjhman44 <sjhman44@users.noreply.github.com>
2019-11-12 15:39:34 -0500

    Update sniffer.c

--- lab_code/sniffer.c
+++ lab_code/sniffer.c
@@ -6,10 +6,29 @@ We can process each packet inside the function.
 void got_packet(u_char *args, const struct pcap_pkthdr *header,
 const u_char *packet)
 {
-printf("Got a packet\n");
+printf("Source IP Address: %d \n Destination IP Address: %d\n",iph_sourceip, iph_destip);
 }
-int main()
-{
+
+/* IP Header */
+struct ipheader {
+  unsigned char      iph_ihl:4, //IP header length
+                     iph_ver:4; //IP version
+  unsigned char      iph_tos; //Type of service
+  unsigned short int iph_len; //IP Packet length (data + header)
+  unsigned short int iph_ident; //Identification
+  unsigned short int iph_flag:3, //Fragmentation flags
+                     iph_offset:13; //Flags offset
+  unsigned char      iph_ttl; //Time to Live
+  unsigned char      iph_protocol; //Protocol type
+  unsigned short int iph_chksum; //IP datagram checksum
+  struct  in_addr    iph_sourceip; //Source IP address 
+  struct  in_addr    iph_destip;   //Destination IP address 
+};
+
+
+
+int main(){             
+                
 pcap_t *handle;
 char errbuf[PCAP_ERRBUF_SIZE];
 struct bpf_program fp;
