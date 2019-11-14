ce854f2a96f732aa668d312ea0e1e23885e14c5f
sjhman44 <sjhman44@users.noreply.github.com>
2019-11-14 15:44:15 -0500

    Update sniff_spoof.py

--- lab_code/sniff_spoof.py
+++ lab_code/sniff_spoof.py
@@ -5,7 +5,7 @@ def print_pkt(pkt):
         pkt.show()
 
 def main():
-        rec_pkt = sniff(filter='icmp',prn=print_pkt)
+        rec_pkt = sniff(iface='ether1',filter='icmp',prn=print_pkt)
         a = IP()
         a[IP].dst =  rec_pkt.getlayer(IP).src
         a[IP].src =  rec_pkt.getlayer(IP).dst
