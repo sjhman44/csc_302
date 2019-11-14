f65eafee2caf08b5fff456b5fd6482cc349cd71a
FC868370 <FC868370@wcupa.edu>
2019-11-14 16:14:07 -0500

    Update sniff_spoof.py

--- lab_code/sniff_spoof.py
+++ lab_code/sniff_spoof.py
@@ -16,3 +16,31 @@ def main():
 if __name__ == "__main__":
     main()
 
+# # receive a packet
+# while True:
+# 	packet = s.recvfrom(65565)
+	
+# 	#packet string from tuple
+# 	packet = packet[0]
+	
+# 	#take first 20 characters for the ip header
+# 	ip_header = packet[0:20]
+	
+# 	#now unpack them :)
+# 	iph = unpack('!BBHHHBBH4s4s' , ip_header)
+	
+# 	version_ihl = iph[0]
+# 	version = version_ihl &gt;&gt; 4
+# 	ihl = version_ihl &amp; 0xF
+	
+# 	iph_length = ihl * 4
+	
+# 	ttl = iph[5]
+# 	protocol = iph[6]
+# 	s_addr = socket.inet_ntoa(iph[8]);
+# 	d_addr = socket.inet_ntoa(iph[9]);
+	
+# 	print 'Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + 
+#         ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + 
+#         ' Destination Address : ' + str(d_addr)
+
